ac26ad73cb9506483c5388b1574f0d456a50a6bf
Rishabh <rjain_be19@thapar.edu>
2021-01-16 08:09:32 +0530

    add page load progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,13 @@ import Layout from "../components/Layout";
 import themeOBJ from "../theme/index";
 import useLocalStorage from "../util/hooks/useLocalStorage";
 import { io } from "socket.io-client";
-
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 import UnitContext from "../util/context/UnitContext";
 export default function MyApp(props) {
   const { Component, pageProps } = props;
